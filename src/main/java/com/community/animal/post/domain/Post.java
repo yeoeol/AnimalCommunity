68669516cf5952dd5a60a5ea8c2e99b7f0446d5a
yeoeol <gnsl3528@naver.com>
2025-01-25 00:55:03 +0900

    [Refactor] User, Post 클래스 수정

--- src/main/java/com/community/animal/post/domain/Post.java
+++ src/main/java/com/community/animal/post/domain/Post.java
@@ -5,6 +5,9 @@ import static jakarta.persistence.FetchType.*;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.community.animal.post.dto.PostRequest;
 import com.community.animal.post.dto.PostResponse;
@@ -12,23 +15,27 @@ import com.community.animal.user.domain.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
-@NoArgsConstructor
-@Getter @Setter
 public class Post {
 
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "post_id")
 	private Long postId;
 
@@ -46,7 +53,16 @@ public class Post {
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@CreationTimestamp
+	@CreatedDate
 	private LocalDateTime regDate;
 
+	@Builder
+	public Post(String postTitle, String postContent, Long postLike, Long postHit, PostCategory postCategory, User user) {
+		this.postTitle = postTitle;
+		this.postContent = postContent;
+		this.postLike = postLike;
+		this.postHit = postHit;
+		this.postCategory = postCategory;
+		this.user = user;
+	}
 }
--- src/main/java/com/community/animal/user/domain/User.java
+++ src/main/java/com/community/animal/user/domain/User.java
@@ -2,11 +2,16 @@ package com.community.animal.user.domain;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -16,12 +21,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class User {
 
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private Long userId;
 
@@ -32,15 +38,14 @@ public class User {
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
+	@CreatedDate
 	private LocalDateTime createDate;
 
 	@Builder
-	public User(Long userId, String username, String password, String email, Role role, LocalDateTime createDate) {
-		this.userId = userId;
+	public User(String username, String password, String email, Role role) {
 		this.username = username;
 		this.password = password;
 		this.email = email;
 		this.role = role;
-		this.createDate = createDate;
 	}
 }
